3ebf02a4ed0b2f482ddebc90cc21e270eae1c623
sasikumar <sasikumarraman1672@gmail.com>
2022-04-04 05:32:45 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import React from "react";
+import Dashboard from "./Dashbord";
+
+const App = () => {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Dashbord.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Dashbord() {
+  return (
+    <div className="Dashbord">
+      <h3>sasi</h3>
+    </div>
+  );
+}
+
+export default Dashbord;
